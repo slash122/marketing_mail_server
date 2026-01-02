5cdc98c9ec14e77464dedbff1859160cb15dd4ce
Gleb Avrutin <gleb.avrutin@gmail.com>
2026-01-02 17:38:43 +0100

    Adjustments

--- backend/marketing_api/api/router.py
+++
@@ -1,5 +0,0 @@
-from fastapi import APIRouter
-from api.endpoints import test
-
-router = APIRouter()
-router.include_router(test.router, prefix="/test", tags=["test"])
--- backend/marketing_api/main.py
+++ backend/marketing_api/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from api.router import router as api_router
+from src.api.router import router as api_router
 
 app = FastAPI(version="0.1")
 app.include_router(api_router)
